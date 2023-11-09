206318732c1b4765469812c989e9803b031b3941
Kiran MC <kiranmc12@gmail.com>
2023-11-09 19:11:01 +0530

    This is my first commit

---
+++ lib/scrn2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class hey extends StatelessWidget {
+  const hey({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:SingleChildScrollView() ,
+    );
+  }
+}
